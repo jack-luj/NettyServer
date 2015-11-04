dc2fbd682f4c58d03731ea3f2ed29b060cfad588
jack-luj <wjsc@qq.com>
2015-11-04 16:27:11 +0800

    补充

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,59 @@
 package com.company;
 
+import io.netty.buffer.ByteBuf;
+
+import java.util.Date;
+
+import static io.netty.buffer.Unpooled.buffer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Long start=new Date().getTime();
+        Long now=start;
+        now=new Date().getTime();
+        while (now-start<=30000) {
+            try {
+                Thread.sleep(5000);
+                System.out.println("haha");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            now=new Date().getTime();
+        }
+
+
+
+
+        // write your code here
+        //造ack数据
+        ByteBuf bb=buffer(1024);
+        bb.writeByte(0x23);//0
+        bb.writeByte(0x23);//1
+        bb.writeShort(0x4d40);//2-3
+        bb.writeByte(0x00);//4
+        bb.writeInt(0x55D20FE7);//5-8
+        bb.writeByte(0x42);//9
+        bb.writeByte(0x02);//10
+        bb.writeBytes(new byte[]{0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35});//11-25
+        bb.writeBytes(new byte[]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});//26-32
+        bb.writeInt(0x55BEE258);//33-36 eventId 1438573144
+        bb.writeByte(1);//37
+        bb.writeByte(3);//38 个数
+        //data
+        bb.writeByte(1);//39
+        bb.writeBytes(new byte[]{0x61, 0x62, 0x63, 0x64, 0x65, 0x66});//40-43
+        bb.writeByte(2);//39
+        bb.writeBytes(new byte[]{0x61});//40-43
+        bb.writeByte(3);//39
+        bb.writeBytes(new byte[]{0x61, 0x62, 0x63, 0x64, 0x65});//40-43
+        bb.writeByte(0x5D);//checksum
+
+
+
+
+        String byteStr=DataTool.getStringFromByteBuf(bb);
+        System.out.println(byteStr);
     }
 }
---
+++ src/com/company/TaskThread.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by luj on 2015/11/4.
+ */
+public class TaskThread implements Runnable {
+    private String jobName = "";
+
+    public TaskThread(String jobName) {
+        super();
+        this.jobName = jobName;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(new Date().toString()+"execute " + jobName);
+    }
+
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+
+        long initialDelay1 = 1;
+        long period1 = 1;
+        // 从现在开始1秒钟之后，每隔1秒钟执行一次job1
+        service.scheduleAtFixedRate(
+                new TaskThread("job1"), initialDelay1,
+                period1, TimeUnit.SECONDS);
+
+        long initialDelay2 = 1;
+        long delay2 = 1;
+        // 从现在开始2秒钟之后，每隔2秒钟执行一次job2
+        service.scheduleWithFixedDelay(
+                new TaskThread("job2"), initialDelay2,
+                delay2, TimeUnit.SECONDS);
+    }
+}
