d8dc0a38be4d9159d5146dfa78ecec5b7760181c
jack-luj <wjsc@qq.com>
2016-03-24 17:58:47 +0800

    字号控制

--- src/com/study/keyboard/KeyboardDemo.java
+++ src/com/study/keyboard/KeyboardDemo.java
@@ -73,85 +73,78 @@ public class KeyboardDemo extends JFrame {
             @Override
             public void keyTyped(KeyEvent e) {
             }
+
             @Override
             public void keyPressed(KeyEvent e) {
-               if(e.isControlDown()&&e.getKeyCode()== KeyEvent.VK_DOWN){
-                   panel.sleep=panel.sleep-10<10?10:panel.sleep-10;
-               }
-               else if(e.isControlDown()&&e.getKeyCode()==KeyEvent.VK_UP){
-                   panel.sleep=panel.sleep+10<10?10:panel.sleep+10;
+                if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    panel.sleep = panel.sleep - 10 < 10 ? 10 : panel.sleep - 10;
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_UP) {
+                    panel.sleep = panel.sleep + 10 < 10 ? 10 : panel.sleep + 10;
                 }
 
-                if(e.isControlDown()&&e.getKeyCode()== KeyEvent.VK_LEFT){
-                    panel.count=panel.count-5<5?5:panel.count-5;
+                if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    panel.count = panel.count - 5 < 5 ? 5 : panel.count - 5;
                     initXY();
-                }
-                else if(e.isControlDown()&&e.getKeyCode()==KeyEvent.VK_RIGHT){
-                    panel.count=panel.count+5<10?10:panel.count+5;
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    panel.count = panel.count + 5 < 10 ? 10 : panel.count + 5;
                     initXY();
-                }
-
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_R){
-                    panel.fColor=Color.red;
-                    fColor=Color.red;
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_R) {
+                    panel.fColor = Color.red;
+                    fColor = Color.red;
                     jTextField.setForeground(fColor);
                     jLabel.setForeground(fColor);
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_G){
-                    panel.fColor=Color.green;
-                    fColor=Color.green;
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_G) {
+                    panel.fColor = Color.green;
+                    fColor = Color.green;
                     jTextField.setForeground(fColor);
                     jLabel.setForeground(fColor);
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_Y){
-                    panel.fColor=Color.yellow;
-                    fColor=Color.yellow;
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_Y) {
+                    panel.fColor = Color.yellow;
+                    fColor = Color.yellow;
                     jTextField.setForeground(fColor);
                     jLabel.setForeground(fColor);
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_0){
-                  panel.stringRandom=false;
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_1){
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_0) {
+                    panel.stringRandom = false;
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_1) {
                     jTextField.setText("我们路过高山路过湖泊");
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_2){
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_2) {
                     jTextField.setText("办证13912345678");
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_3){
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_3) {
                     jTextField.setText("▲");
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_4){
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_4) {
                     jTextField.setText("☆");
-                }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_5){
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_5) {
                     jTextField.setText("★");
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_ESCAPE){//清空
+                } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {//清空
                     jTextField.setText("");
-                }
-                else if(e.isControlDown()&&e.getKeyCode()==KeyEvent.VK_P){//暂停/继续
-                    if(panel.isSuspend==0){
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_P) {//暂停/继续
+                    if (panel.isSuspend == 0) {
                         t1.suspend();
-                        panel.isSuspend=1;
-                    }else{
+                        panel.isSuspend = 1;
+                    } else {
                         t1.resume();
-                        panel.isSuspend=0;
+                        panel.isSuspend = 0;
                     }
 
+                } else if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_ENTER) {//
+                    //   jLabel.setText( jTextField.getParent().getParent().getParent().toString());
+                    e.getComponent().getParent().getParent().getParent().setBackground(Color.blue);
                 }
-                else if(e.isAltDown()&&e.getKeyCode()==KeyEvent.VK_ENTER){//
-                 //   jLabel.setText( jTextField.getParent().getParent().getParent().toString());
-               e.getComponent().getParent().getParent().getParent().setBackground(Color.blue);
+
+                if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_PAGE_UP) {
+                    panel.fontSize=panel.fontSize+1;
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_PAGE_DOWN) {
+                    panel.fontSize=panel.fontSize-1;
                 }
+
 
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                String txt=jTextField.getText();
+                String txt = jTextField.getText();
                 jLabel.setText(txt);
-                panel.txt=txt;
+                panel.txt = txt;
 
             }
         });
@@ -171,11 +164,12 @@ public class KeyboardDemo extends JFrame {
         private int[] speeds;
         private int[] startX;
         private int[] startY;
+        private int fontSize=16;
         public void paintComponent(Graphics g,double leftX,  double topY ) {
             super.paintComponent(g);
             g.setColor(fColor);
-            g.setFont(new Font("", Font.ROMAN_BASELINE, 16));
-            g.drawString("这是最后一行文本 当前字体行高:" + g.getFontMetrics().getHeight() + "  窗体宽度:" + this.getWidth()+ "  字条个数:" + count+" 延时："+sleep, 0, this.getHeight() - 5);
+            g.setFont(new Font("", Font.ROMAN_BASELINE, fontSize));
+            g.drawString("字号:" + panel.fontSize +" 行高:" + g.getFontMetrics().getHeight() + "  窗体宽度:" + this.getWidth()+ "  字串个数:" + count+"  延时:"+sleep, 0, this.getHeight() - 5);
             if(txt.trim().length()==0){
             return;
             }
@@ -194,14 +188,14 @@ public class KeyboardDemo extends JFrame {
         public void drawTxtArray(String text,Graphics g,int x,int y,int index){//竖向绘字符串
             Graphics2D g2=(Graphics2D) g;
            // g.setFont(new Font("", Font.ROMAN_BASELINE, Tools.getNoBetween(14,40)));
-            g.setFont(new Font("", Font.ROMAN_BASELINE, 16));
+            g.setFont(new Font("", Font.ROMAN_BASELINE, fontSize));
             char[] chars=getChars(text);
             for(int i=0;i<chars.length;i++){
             if(y+g.getFontMetrics().getHeight()*i<=this.getHeight()-g.getFontMetrics().getHeight()){
 
                 //g2.rotate(1,1.0, Tools.getNoBetween(1,10)*1.0);
                 g.drawString(String.valueOf(chars[i]), x, y + g.getFontMetrics().getHeight() * i);
-                //x=x+15;//斜着绘文字
+                x=x+15;//斜着绘文字
                 }
                 }
             startY[index]=startY[index]+speeds[index];
