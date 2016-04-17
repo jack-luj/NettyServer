5222ffdfd1bb6b201e39ab8262847d85a51a37fd
jack_l <wjsc@qq.com>
2016-04-17 17:40:24 +0800

    暂存

--- src/com/study/timer/TimerShow.java
+++ src/com/study/timer/TimerShow.java
@@ -1,7 +1,109 @@
 package com.study.timer;
 
+import javax.swing.*;
+
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
 /**
  * Created by Administrator on 2016/4/17.
  */
-public class TimerShow {
+public class TimerShow extends JPanel implements Runnable{
+
+    private static Color bColor=Color.black;//统一背景色
+    private static Color fColor=Color.RED;//前景色
+    private Date deadDate=new Date();
+    private Date currentDate=new Date();
+    private int fontSize=30;
+
+    public static void main(String[] args) {
+        TimerShow panel = new TimerShow();
+        panel.setBackground(bColor);
+        panel.deadDate=new Date(new Date().getTime()+10*1000);
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setTitle("TimerShow");
+        frame.setContentPane(panel);
+        Thread thread=new Thread(panel);
+        thread.start();
+
+    }
+public TimerShow(){
+
+}
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        painText(g);
+
+    }
+    public void repaint(){
+        super.repaint();
+        getXY();
+    }
+    private String showTxt="2016-04-17 00:00:00";
+    private String countText="00:01:20:123";
+    private int x=100;
+    private int y=100;
+    public void painText(Graphics g) {
+        g.setColor(fColor);
+        g.setFont(new Font("", Font.ROMAN_BASELINE, fontSize));
+        String deadLineStr=formatDate(deadDate);
+        g.drawString("发射时间:"+deadLineStr,x,y-50);
+        showTxt=formatDate(currentDate);
+        g.drawString("现在时间:"+showTxt,x,y);
+        long count=deadDate.getTime()-currentDate.getTime()<0?0:deadDate.getTime()-currentDate.getTime();
+        countText=formatCountDate(count);
+       // g.setFont(new Font("", Font.ROMAN_BASELINE, fontSize+20));
+        g.drawString("倒计时:"+countText,x+100,y+100);
+    }
+
+    public void getXY() {
+        int width=this.getWidth();
+        int height=this.getHeight();
+        x=(width-450)/2;
+        y=height/2;
+        System.out.println(x+"-"+y);
+    }
+    String format="yyyy-MM-dd HH:mm:ss.SSS";
+    public String formatDate(Date date){
+        return new SimpleDateFormat(format).format(date);
+    }
+    String countFormat="HH:mm:ss.SSS";
+    public String formatCountDate(long t){
+        Date date=new Date(t);
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(countFormat);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+
+        return simpleDateFormat.format(date);
+    }
+
+    @Override
+    public void run() {
+
+        while (currentDate.getTime()<=deadDate.getTime()) {
+            System.out.println(currentDate.toLocaleString());
+            currentDate = new Date();
+            repaint();
+        }
+        String temp=countText;
+        try{
+            for (int i=0;i<5;i++){
+                System.out.println(">>>>>>>>>>>>>..");
+                fColor=Color.black;
+                repaint();
+                Thread.sleep(500);
+                fColor=Color.RED;
+                repaint();
+                Thread.sleep(500);
+            }
+        }catch (InterruptedException e){e.printStackTrace();}
+
+    }
+
+
 }
