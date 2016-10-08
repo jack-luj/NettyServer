6da5635dc211544cce4489dc4758a10a224c2965
jack-luj <wjsc@qq.com>
2016-10-08 17:34:41 +0800

    md5

--- src/com/MQTT/MD5Util.java
+++ src/com/MQTT/MD5Util.java
@@ -1,32 +1,39 @@
 package com.MQTT;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MD5Util {
-    public final static String MD5(String s) {
-        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+    /**
+     * MD5 16位大写
+     * @param sourceStr
+     * @return
+     */
+    private static String MD5To16UpperCase(String sourceStr) {
+        String result = "";
         try {
-            byte[] btInput = s.getBytes();
-            // 获得MD5摘要算法的 MessageDigest 对象
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-            // 使用指定的字节更新摘要
-            mdInst.update(btInput);
-            // 获得密文
-            byte[] md = mdInst.digest();
-            // 把密文转换成十六进制的字符串形式
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(sourceStr.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
             }
-            return new String(str);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            result = buf.toString().substring(8, 24).toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e);
         }
+        return result;
     }
+
+
     public static void main(String[] args) {
-        System.out.println(MD5Util.MD5("20121221"));
+        System.out.println(MD5Util.MD5To16UpperCase("12345678919912345678919991234"));
        }
 }
--- src/com/study/AES128Tool.java
+++ src/com/study/AES128Tool.java
@@ -6,6 +6,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 /**
  * Created by jackl on 2016/9/23.
@@ -21,12 +22,8 @@ public class AES128Tool {
      */
     public static byte[] encrypt(String content, String password) {
         try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, new SecureRandom(password.getBytes()));
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
+
             SecretKeySpec key = new SecretKeySpec(password.getBytes(), "AES");
-            //Cipher cipher = Cipher.getInstance("AES");// 创建密码器
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");// 创建密码器
             byte[] byteContent = content.getBytes("utf-8");
             cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
@@ -48,15 +45,26 @@ public class AES128Tool {
         return null;
     }
 
+
     public static byte[] encrypt(byte[] byteContent, String password) {
+        byte[] pwdBytes=password.getBytes();
+        if(password.length()<16) {
+            pwdBytes=Arrays.copyOf(pwdBytes, 16);
+            Arrays.fill(pwdBytes, password.length(),16,(byte) 0xff);
+        }
+        System.out.println("AA:"+parseByte2HexStr(pwdBytes));
+        return encrypt(byteContent,pwdBytes);
+    }
+
+    /**
+     *
+     * @param byteContent 需要加密的内容
+     * @param password 加密密码
+     * @return
+     */
+    public static byte[] encrypt(byte[] byteContent, byte[] password) {
         try {
-        /*    KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, new SecureRandom(password.getBytes()));
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();*/
-            byte[] raw = password.getBytes();
-            SecretKeySpec key = new SecretKeySpec(raw, "AES");
-            //Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            SecretKeySpec key = new SecretKeySpec(password, "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");// 创建密码器
             cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
             byte[] result = cipher.doFinal(byteContent);
@@ -75,19 +83,26 @@ public class AES128Tool {
         return null;
     }
 
+
+    public static byte[] decrypt(byte[] content, String password) {
+        byte[] pwdBytes=password.getBytes();
+        if(password.length()<16) {
+            pwdBytes=Arrays.copyOf(pwdBytes, 16);
+            Arrays.fill(pwdBytes, password.length(),16,(byte) 0xff);
+        }
+        return decrypt(content, pwdBytes);
+    }
+
     /**解密
      * @param content  待解密内容
      * @param password 解密密钥
      * @return
      */
-    public static byte[] decrypt(byte[] content, String password) {
+    public static byte[] decrypt(byte[] content, byte[] password) {
         try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, new SecureRandom(password.getBytes()));
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(password.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+
+            SecretKeySpec key = new SecretKeySpec(password, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");// 创建密码器
             cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
             byte[] result = cipher.doFinal(content);
             return result; // 加密
@@ -105,15 +120,18 @@ public class AES128Tool {
         return null;
     }
 
+
+
     public static void main(String[] args){
        //不满16字节加密后长度 16
         //16字节加密后数据长度 32
         String content = "1234567812345678";
-        String password = "1234567887654321";
+        String password = "12345678876543";
         //加密
         System.out.println("明文：" + content);
         System.out.println("密钥：" + password);
-        byte[] encryptResult = encrypt(content, password);
+        //byte[] encryptResult = encrypt(content, password);
+        byte[] encryptResult = encrypt(content.getBytes(), password);
         System.out.println("密文："+parseByte2HexStr(encryptResult));
         //解密
        byte[] decryptResult = decrypt(encryptResult,password);
